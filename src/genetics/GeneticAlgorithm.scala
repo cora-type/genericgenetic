396ec0a50bb910a61e21b1a5937439db94d787e3
fuckcole <gtsbyxx@gmail.com>
2019-03-29 01:52:57 -0400

    dry6

--- src/genetics/GeneticAlgorithm.scala
+++ src/genetics/GeneticAlgorithm.scala
@@ -5,27 +5,29 @@ import genetics.genes._
 
 object GeneticAlgorithm {
 
-  def linearRegression[Line](input: List[Point]): Line = {
+  def linearRegression[(input: List[Point]): Line = {
     val x: List[Gene] = List(new Gene(.6), new Gene(.7))
-    geneticAlgorithm(distance(input), geneToLine(x), x)
+    geneticAlgorithm(fitFunc(input), x => geneToLine(x) , x)
   }
 
-  def distance(inputData: List[Point]): Line => Double = {
-    line: Line => {
-      var sum: Double = 0
+
+
+  def fitFunc(inputData: List[Point]): Line => Double = {
+    lineInput: Line => {
+      var total: Double = 0
       for (points <- inputData){
-        sum += Math.abs(points.y) - Math.abs(line.evaluate(points.x))
+        total = total + Math.abs(points.y) - Math.abs(lineInput.evaluate(points.x)) // evaluates to see how well line fits the points, closer the better
       }
-      sum
+      total
     }
   }
 
   def geneToLine(inputData: List[Gene]) : Line = {
-    var x = new Line(inputData(0).geneValue, inputData(1).geneValue)
+    val x = new Line(inputData(0).geneValue, inputData(1).geneValue)
+    x
   }
 
-  def geneticAlgorithm[Line](distant: Line => Double, geneToL: List[Gene] => Line, list:List[Gene]): Line ={
-
+  def geneticAlgorithm[T](fitFunc: T => Double, geneToL: List[Gene] => T, list:List[Gene]): T ={
+    new T
   }
-
 }
--- src/tests/TestLinearRegression.scala
+++ src/tests/TestLinearRegression.scala
@@ -7,8 +7,8 @@ import genetics.GeneticAlgorithm
 class TestLinearRegression extends FunSuite {
   test("Yeet"){
     val listOfPoints: List[Point] = List(new Point(4,5), new Point(1,2))
-    val actual: Line = GeneticAlgorithm.linearRegression(listOfPoints)
+    val actual: Line = GeneticAlgorithm.linearRegression[Line](listOfPoints)
 
-    assert(actual. == actual)
+    assert(actual== actual)
   }
 }
